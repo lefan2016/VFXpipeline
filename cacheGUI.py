e8b8b284453a367ee77b99b95ebfc96c8c9a279f
chordee <chordee@gmail.com>
2017-07-19 22:39:54 +0800

    update ViewWIdget

--- cacheGUI.py
+++ cacheGUI.py
@@ -4,7 +4,7 @@ from PySide.QtCore import *
 from PySide.QtGui import *
 
 class MainWidget(QWidget):
-	def __init__(self, cacheDrive = 'Q:', parent = None, projRegex = '^\d{6}\w+'):
+	def __init__(self, cacheDrive = 'Q:', parent = None, projRegex = '^\d{6}\w\F'):
 		super(MainWidget, self).__init__(parent)
 		self.__cacheDrive = cacheDrive
 		self.initUI(projRegex = projRegex)
@@ -15,18 +15,30 @@ class MainWidget(QWidget):
 		self.setWindowTitle('VFX Pipeline Tool')
 		self.resize(860,540)
 
-
 		top_hlayout = QHBoxLayout()
 		main_vlayout = QVBoxLayout()
-
-		
 
 		self.projects_cb = QComboBox()
 		self.cuts_cb = QComboBox()
 
-		self.refresh_projects_cb()
+		projects = []
+		path = os.path.join(self.__cacheDrive)
+		for direct in os.listdir(path):
+			if os.path.isdir(os.path.join(path, direct)) and self.__projRegex.match(direct):
+				projects += [direct]
+		self.projects_cb.addItems(projects)
 
-		self.refresh_cuts_cb()
+		self.pick_project()
+
+		self.cuts_cb.clear()
+		cuts = []
+		for cut in cc.collect(self.projectItem, 'CUT'):
+			cuts += [cut.name()]
+		self.cuts_cb.addItems(cuts)
+
+		self.pick_cut()
+
+		self.view_widget = ViewWidget(item = self.cutItem)
 
 		label = QLabel('Project:')
 		label.setFixedWidth(40)
@@ -37,31 +49,48 @@ class MainWidget(QWidget):
 		top_hlayout.addWidget(label)
 		top_hlayout.addWidget(self.cuts_cb)
 		main_vlayout.addLayout(top_hlayout)
+		main_vlayout.addWidget(self.view_widget)
 
 		self.setLayout(main_vlayout)
 
 		self.projects_cb.currentIndexChanged.connect(self.pick_project)
+		self.cuts_cb.currentIndexChanged.connect(self.pick_cut)
 
 	def pick_project(self):
+		self.projectItem  = cc.ProjectCahce(project = self.projects_cb.currentText())		
 		self.refresh_cuts_cb()
 
+	def pick_cut(self):
+		cut = self.cuts_cb.currentText()
+		if cut != '':
+			cuts = [x for x in self.projectItem.children() if x.name() == cut ]
+			self.cutItem = cuts[0]
+		else:
+			self.cutItem = None
+
 	def refresh_cuts_cb(self):
-		self.projectItem  = cc.ProjectCahce(project = self.projects_cb.currentText())
 		self.cuts_cb.clear()
 		cuts = []
 		for cut in cc.collect(self.projectItem, 'CUT'):
 			cuts += [cut.name()]
 		self.cuts_cb.addItems(cuts)
 
-	def refresh_projects_cb(self):
-		projects = []
-		path = os.path.join(self.__cacheDrive)
-		for direct in os.listdir(path):
-			if os.path.isdir(os.path.join(path, direct)) and self.__projRegex.match(direct):
-				projects += [direct]
-		self.projects_cb.clear()
-		self.projects_cb.addItems(projects)
 
+
+
+
+class ViewWidget(QListWidget):
+	def __init__(self, item, parent = None):
+		super(ViewWidget, self).__init__(parent)
+		self.cutItem = item
+		self.initUI()
+
+	def initUI(self):
+		print self.cutItem
+		if self.cutItem != None:
+			print self.cutItem.name()
+			for i,j in enumerate(self.cutItem.children()):
+				self.insertItem(i, j.cacheName())
 
 
 if __name__ == '__main__':
